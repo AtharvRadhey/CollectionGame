4661a47bed6afd7798a369f5961a4b9052beaf92
AtharvRadhey <76675130+AtharvRadhey@users.noreply.github.com>
2021-02-20 23:26:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ boy = createSprite(width/2,height-20,20,20);
 boy.addAnimation("SahilRunning",boyImg);
 boy.scale=0.08;
   
-  boy.setCollider("rectangle",0,0,2000,2000);
+ boy.setCollider("rectangle",0,0,2000,2000);
  boy.debug=false; 
   
 cashG=new Group();
@@ -109,7 +109,7 @@ function createCash() {
   cash.addImage(cashImg);
   cash.scale=0.12;
   cash.velocityY = 3;
-  cash.lifetime = 150;
+  cash.lifetime = 200;
   cashG.add(cash);
   }
 }
@@ -120,7 +120,7 @@ function createDiamonds() {
   diamonds.addImage(diamondsImg);
   diamonds.scale=0.03;
   diamonds.velocityY = 3;
-  diamonds.lifetime = 150;
+  diamonds.lifetime = 200;
   diamondsG.add(diamonds);
 }
 }
@@ -131,7 +131,7 @@ function createJwellery() {
   jwellery.addImage(jwelleryImg);
   jwellery.scale=0.13;
   jwellery.velocityY = 3;
-  jwellery.lifetime = 150;
+  jwellery.lifetime = 200;
   jwelleryG.add(jwellery);
   }
 }
@@ -142,7 +142,7 @@ function createSword(){
   sword.addImage(swordImg);
   sword.scale=0.1;
   sword.velocityY = 3;
-  sword.lifetime = 150;
+  sword.lifetime = 200;
   swordGroup.add(sword);
   }
 }
